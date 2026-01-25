971033ed1e0e8e3e7c6b18f57ee053ff70d63296
Eamon2009 <eamon112009@gmail.com>
2026-01-25 10:24:05 +0530

    Update 03-code.py

--- Dictionary/03-code.py
+++ Dictionary/03-code.py
@@ -1,16 +1,16 @@
-phone=dict()
-i=1
-n=int(input("Enter number of enteries:"))
-while i<=n:
-    a=input("Enter Name:")
-    b=input("Enter phone no:")
-    phone[a]=b
-    i=i+1
-l=phone.keys()
-x=int(input("Enter the name to be searched:"))
-for i in l:
-    if i==x:
-        print(x,": phone no =",phone[i])
-        break
-    else:
-        print(x,"not exist......")
+phone = dict()
+n = int(input("Enter number of entries: "))
+
+# Collecting data
+for i in range(n):
+    a = input("Enter Name: ")
+    b = input("Enter phone no: ")
+    phone[a] = b
+
+# Searching data
+x = input("Enter the name to be searched: ") # Removed int() conversion
+
+if x in phone:
+    print(x, ": phone no =", phone[x])
+else:
+    print(x, "does not exist.")
