86abc8d3f33c03c63f00d44cdaee16b9fdd19c7b
Eamon2009 <eamon112009@gmail.com>
2026-02-04 20:43:29 +0530

    Update 01class.py

--- class_/01class.py
+++ class_/01class.py
@@ -1,3 +1,4 @@
+import math
 class calculator:
        def __init__(self,n):
               self.n=n
@@ -6,7 +7,15 @@ class calculator:
        def cube(self):
               print(f"The cube of number={self.n*self.n*self.n}")
        def square_root(self):
-              print(f"The square root of the number={self.n**(1/2)}")
+              print(f"The square root of the number={math.sqrt(self.n)}")
+@ staticmethod # for now it is 
+def factorial(n):
+       if n==1 or n==0:
+              return 1
+       else:
+              return n*factorial(n-1)
+n=int(input(">>>"))
+print(f"The staticmetod work,output {factorial(n)}")
 try:
        userint=int(input(">>>"))
 except ValueError:
