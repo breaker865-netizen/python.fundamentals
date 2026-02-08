9400adc71857b74d89a3c2ba762191ef23c20507
Eamon2009 <eamon112009@gmail.com>
2026-02-08 11:13:05 +0530

    password generator

---
+++ password_generator.py
@@ -0,0 +1,22 @@
+import random
+stri="1234567890!@#$%^&*()-_=+qwertyuiop[]}\|asdfghjkl:'zxcvbnm<>?,./QWERTYUIOPASDFGHJKLZXCVBNM'"
+
+while True:
+       try:
+              a=input("password of what [for mail,facebook,bank etc]:")
+              a1=str(a)
+              lenght=int(input("Enter lenght of the password:"))
+              password=""
+              for a in range(lenght):
+                     password+=random.choice(stri)
+              print(password)
+              with open("data.txt", "a") as file:
+                file.write(f"{a1} password is :{password}\n")
+                
+              print(f"✅ Generated and saved: {password}")
+              print(f"Stored in data.txt")
+
+            
+
+       except ValueError:
+              print("Invalid Input!!")
