69092d7d61dc2557b818a26c1f1d4891700fdf0a
Eamon2009 <eamon112009@gmail.com>
2026-02-12 23:23:56 +0530

    Create star_pattern.py

---
+++ star_pattern.py
@@ -0,0 +1,13 @@
+'''
+Docstring for star_pattern
+Triangel patters using stars * 
+Using 6 Rows
+'''
+def star_triange(row):
+              for i in range(row):
+                     for j in range(row-i-1):
+                            print(" ",end="")
+                     for j in range(2*i+1):
+                            print("*",end="")
+                     print()
+star_triange(6)
