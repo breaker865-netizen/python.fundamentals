5cf01fde2b39fcf50d2c3bf16b6bfb30577dc8fd
Eamon <eamon112009@gmail.com>
2026-02-20 11:09:11 +0530

    Create 06code.py

---
+++ loops/06code.py
@@ -0,0 +1,16 @@
+#Reverse a number using a while loop.
+while True:
+     irt=input("Enter your choice:").lower()
+     if irt=='check':
+          try:
+               num=int(input("Enter a number:"))
+               while num>0:
+                digit = num % 10
+                reverse = reverse * 10 + digit
+                num = num // 10
+               print(reverse)
+          except ValueError:
+              print("Invalid!!")
+     else:
+          print("-------")
+          break
