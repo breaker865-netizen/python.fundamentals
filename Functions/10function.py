90fa6150c7a1a0292bc7ea8b6f93dc96d28a9327
Eamon <eamon112009@gmail.com>
2026-02-24 20:04:09 +0530

    Update 10function.py

--- Functions/10function.py
+++ Functions/10function.py
@@ -3,3 +3,10 @@ def reverse_string(s):
 text = "hello"
 reversed_text = reverse_string(text)
 print(reversed_text)  # Output: "olleh"
+
+# else
+def reverse_string(s):
+    reversed_str = ""
+    for char in s:
+        reversed_str = char + reversed_str
+    return reversed_str
